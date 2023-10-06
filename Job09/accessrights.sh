2abd2eb7dc9eed36e9f041d01052e2e53da90b81
vanny-laure-lamorte <vanny.lamorte@laplateforme.io>
2023-10-06 20:14:28 +0200

    Envoyer Jo09

--- Job09/accessrights.sh
+++ Job09/accessrights.sh
@@ -1,4 +1,36 @@
 #!/bin/bash
 
+csv_file="/home/vanny/Documents/shell-exe/Job09/Shell_Userlist.csv"
 
+# Lire le fichier CSV
+
+while IFS=',' read -r Id Prenom Nom Mdp Role; do
+echo "Id : $Id"
+echo "Nom complet: $Prenom $Nom"
+echo "Mdp: $Mdp"
+echo "Role: $Role"
+
+# Vérifier si l'utilisateur à déjà 
+nom_utilisateur="${Prenom,,}${Nom}"
+
+if id "$nom_utilisateur" &>/dev/null; then
+echo "L'utilisateur $nom_utilisateur existe déjà"
+
+else
+
+#Crypter les mots de passe et créer des utilisateurs
+Mdpc=$(mkpasswd -m sha-512 "$Mdp")
+sudo useradd -m -p "$Mdpc" "$nom_utilisateur"
+
+#Supprimer l'utilisateur Nom Prenom
+sudo rm -rf /home/prenomNom
+
+# Donner les permissions de super utilisateur aux utilisateurs Admin
+	if [$Role="Admin"];then
+	sudo usermod -aG sudo "$utilisateur" 
+	echo "L'utilisateur $nom_utilisateur est un super utilisateur"
+sudo userdel prenomNom
+
+fi
+done < "$csv_file"
 
